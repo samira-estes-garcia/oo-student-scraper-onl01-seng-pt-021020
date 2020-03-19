f271252d8682ecc76f9e9a2adbe0f30081b9793c
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-19 18:56:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |attribute, value|
-      self.send(#{attribute}=value)
+      self.send("#{attribute}=")
       
     @@all << self
   end
