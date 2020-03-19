725f72da63f52df3b7cb932c33682cd3fbfa5890
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-19 18:58:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
+    students_array.each do |student_hash|
       self.new 
     end
     student
